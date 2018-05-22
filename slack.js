75417449b677ea103cb3d59cbff4f95f85de843a
Thomas Pedbereznak <tom@tomped.com>
2018-05-22 14:01:40 +0200

    Correct query params and better responses

--- slack.js
+++ slack.js
@@ -1,27 +1,23 @@
 const express = require('express');
 const { RTMClient } = require('@slack/client');
 
-const token = process.env.SLACK_TOKEN;
-
 const app = express();
 app.set('port', (process.env.PORT || 5000));
 
-const rtm = new RTMClient(token);
+const rtm = new RTMClient(process.env.BOT_TOKEN);
 rtm.start();
 
 app.post('/send', (req, res) => {
-  if (!req.query || !req.query.messasge || !req.query.coversation_id) {
-    const error_message = {error: 'must provide message and conversation_id via query parameters'};
-    process.stderr.write(error_message);
-    res.status(400).send(error_message);
+  res.setHeader('Content-Type', 'application/json');
+  if (!req.query || !req.query.message || !req.query.conversation_id) {
+    res.status(400).send({error: 'must provide message and conversation_id via query parameters'});
   } else {
     rtm.sendMessage(req.query.message, req.query.conversation_id)
       .then((data) => {
-        res.send('Message sent: ' + data.ts);
+        res.send(data);
       })
       .catch((err) => {
-        process.stderr.write(err);
-        res.status(500).send(err);
+        res.status(400).send(err);
       });
   }
 });

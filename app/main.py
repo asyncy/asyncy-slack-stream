9edfead9082d1a5596939e8741ccf4d03b46ef1c
Steve Peak <steve@stevepeak.net>
2019-02-19 11:19:07 +0100

    fix ports

--- app/main.py
+++ app/main.py
@@ -3,10 +3,15 @@
 from app import server
 import api
 import bot
+import os
 
 
 if __name__ == '__main__':
     try:
-        server.run(host='0.0.0.0', port=8000, debug=True)
+        server.run(
+            host=os.getenv('FLASK_HOST', '0.0.0.0'),
+            port=int(os.getenv('FLASK_PORT', 8000)),
+            debug=(os.getenv('FLASK_DEBUG') == 'TRUE')
+        )
     except:
         bot.rtm.stop()
